5b7f6c826cf2dff2a3506d8bbff8fa4ecb3efdfd
KoYujung <117340853+KoYujung@users.noreply.github.com>
2023-01-19 18:30:54 +0900

    Add files via upload

--- src/Pay_kio.js
+++ src/Pay_kio.js
@@ -7,24 +7,21 @@ export default class Pay_kio extends Component {
       <div>
         <br></br>
         <h1>주문 내역 확인</h1>
+        <br></br>
         <hr></hr>
         <br></br>
         <h2>주문 정보를 확인해 주세요</h2>
         <br></br>
-        <table border={0} cellPadding={0} cellSpacing={30}>
+        <table border={1} cellPadding={50} cellSpacing={0}>
           <tr>
             <td>상품명</td>
             <td>가격</td>
           </tr>
           <tr>
-            <td>{this.props.title}</td>
-            <td>{this.props.desc}</td>
-          </tr>
-          <tr>
-            <td></td>
+            <td className='pay'>{this.props.title}</td>
+            <td className='pay'>{this.props.desc}</td>
           </tr>
         </table>
-        <Link to="/fir"><button>뒤로</button></Link>
         <br></br>
       </div>
     )
